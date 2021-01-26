b637e187eb98c7a268aaf9f5eb837cbba146552c
Mitchell Rodríguez <mitrodle@gmail.com>
2021-01-25 21:51:15 -0500

    Add: JSX

--- src/index.js
+++ src/index.js
@@ -8,13 +8,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Para usar JSX es requisito importar React
-// JSX Es mucho más legible y expresivo
-const jsx = <h1>Hello, Platzi Badges!</h1>;
+// const name = 'Mitch';
 
-const element = React.createElement('a', { href: 'https://platzi.com' }, 'Ir a Platzi');
+// // Para usar JSX es requisito importar React
+// // JSX Es mucho más legible y expresivo
+
+// // Si pasamos {null} {undefined} no se presenta nada
+// const jsx = <h1>Hello, I'm {name}</h1>;
+
+// const element = React.createElement('h1', {}, `Hola, soy ${name}`);
+
+const jsx = <div>
+    <h1>Hola, soy Mitch</h1>
+    <p>Soy un desarrollador Front-End</p>
+</div>
+
+/* Versus React.createElement
+const element = React.createElement(
+    'div',
+    {},
+    React.createElement('h1', {}, 'Hola, soy Mitch'),
+    React.createContext('p', {}, 'Soy un desarrollador Front-End')
+);
+*/
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
